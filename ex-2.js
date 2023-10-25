5527c71d1994abe8ca30c1d1b7e9f32516b777a8
sirithanatharathorntv <sirithanatharathorntv@gmail.com>
2023-10-25 21:24:30 +0700

    create branch pop

--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,20 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (operation(array[i])) {
+      count++;
+      if (count >= 5) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function checkPoint(point) {
+  return point > 70;
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +24,9 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, checkPoint);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, checkPoint);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, checkPoint);
+console.log("Room 1: " + scoreRoom1Result);
+console.log("Room 2: " + scoreRoom2Result);
+console.log("Room 3: " + scoreRoom3Result);
